5b9c3af6203e60a9a4daddacf110cbd0ec484809
mcgear <michael.gearhardt@fathym.com>
2019-06-13 02:18:51 -0600

    Checkpoint

--- Harness/ForgeInfrastructureStateHarness.cs
+++ Harness/ForgeInfrastructureStateHarness.cs
@@ -297,10 +297,6 @@ namespace LCU.State.API.Forge.Infrastructure.Harness
 
             log.LogInformation($"Repository {repoDir.FullName} ensured");
 
-            var usersHomePath = Path.Combine(filesRoot, @"Users\Home");
-
-            var usersHomeDrive = Path.GetPathRoot(usersHomePath).TrimEnd('\\');
-
             var lineCount = 0;
 
             var npm = new System.Diagnostics.Process();
@@ -309,9 +305,7 @@ namespace LCU.State.API.Forge.Infrastructure.Harness
 
             npm.StartInfo.CreateNoWindow = true;
 
-            npm.StartInfo.Environment.Add("HOMEDRIVE", usersHomeDrive);
-
-            npm.StartInfo.Environment.Add("HOMEPATH", usersHomePath.TrimStart(usersHomeDrive.ToArray()));
+            npm.StartInfo.Environment["Path"] = @"D:\home\site\deployments\tools;D:\Program Files (x86)\SiteExtensions\Kudu\82.10503.3890\bin\Scripts;D:\Program Files (x86)\MSBuild\14.0\Bin;D:\Program Files\Git\cmd;D:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow;D:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn;D:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0;D:\Program Files\Git\bin;D:\Program Files\Git\usr\bin;D:\Program Files\Git\mingw64\bin;D:\Program Files (x86)\npm\6.4.1;C:\DWASFiles\Sites\#1lcu-int-state-api-forge-infrastructure\AppData\npm;D:\Program Files (x86)\bower\1.7.9;D:\Program Files (x86)\grunt\0.1.13;D:\Program Files (x86)\gulp\3.9.0.1;D:\Program Files (x86)\funcpack\1.0.0;D:\Program Files (x86)\nodejs\10.14.1;D:\Windows\system32;D:\Windows;D:\Windows\System32\Wbem;D:\Windows\System32\WindowsPowerShell\v1.0\;D:\Users\Administrator\AppData\Local\Microsoft\WindowsApps;;D:\Program Files (x86)\dotnet;D:\Windows\system32\config\systemprofile\AppData\Local\Microsoft\WindowsApps;D:\Program Files (x86)\Git\cmd;D:\Program Files (x86)\PHP\v5.6;D:\Python27;";
 
             npm.StartInfo.FileName = cmdExePath;
 
@@ -362,7 +356,7 @@ namespace LCU.State.API.Forge.Infrastructure.Harness
 
             // npm.StandardInput.WriteLine($"lcu proj {projName} --template={lcuTemplate}");
 
-            appSeed.Commands.ForEach(command =>
+            appSeed.Commands.Take(1).ForEach(command =>
             {
                 lineCount = 0;
 
@@ -379,9 +373,7 @@ namespace LCU.State.API.Forge.Infrastructure.Harness
 
                 log.LogInformation($"Executing command {runCmd}");
 
-                npm.StandardInput.WriteLine(runCmd);
-
-                npm.StandardInput.WriteLine($"exit");
+                npm.StandardInput.WriteLine($"{runCmd} & exit");
 
                 npm.WaitForExit();
 
